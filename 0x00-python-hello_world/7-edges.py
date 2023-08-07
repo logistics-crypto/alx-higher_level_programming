f20f694b4a0875ca81d052f7f86e85209eab10bf
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-07 16:05:27 -0700

    sting

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"First 3 letters: {:3}", end='\n')
-print(f"Last 2 letters: {-2:}", end='\n')
-print(f"Middle word: {1:-1}", end='\n')
+
+print(f"First 3 letters: {word[1:3]}", end='\n')
+
+print(f"Last 2 letters: {word[-2:]}", end='\n')
+
+print(f"Middle word: {word[1:-1]}", end='\n')
--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-start_index = str.find("object-oriented programming")
-print(str[start_index:], end='\n')
+
+print(f"{str[38:66]} {str[107:111]} {str[:6]}", end='\n')
