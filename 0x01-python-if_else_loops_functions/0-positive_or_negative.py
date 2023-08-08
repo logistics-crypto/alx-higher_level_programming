63a90c88d20e6b979fb68ab0ee13f0a20569c5e4
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-08 11:53:59 -0700

    1-last_digit.py commit

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number < 0:
+    print("Negative number:", number)
+elif number > 0:
+    print("Positive number:", number)
+else:
+    print("Zero:", number)
