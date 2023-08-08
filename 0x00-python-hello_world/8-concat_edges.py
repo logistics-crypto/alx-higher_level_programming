3899906f07e2038ff48defe5369aef9fd009ebab
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-07 17:12:52 -0700

    object oriented

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-
-print(f"{str[38:66]} {str[107:111]} {str[:6]}", end='\n')
+        language that combines remarkable power with very clear syntax"
+str2 = str[39:66] + str[113:118]
+print(f"{str2} {str[:6]}")
