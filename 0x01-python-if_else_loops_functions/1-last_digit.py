b9d8ca8f911490663c308bdec5595882ec8ef2b9
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-08 11:58:24 -0700

    1-last_digit.py commit2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,6 +11,8 @@ if last_digit > 5:
     output += " and is greater than 5"
 elif last_digit == 0:
     output += " and is 0"
+elif last_digit < 0
+    output += " and is less than 6 and not 0"
 else:
     output += " and is less than 6 and not 0"
 
