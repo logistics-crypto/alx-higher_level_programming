30e744855108c6c933be72149c711b1bb9fc2e59
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-07 17:15:24 -0700

    7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-
-print(f"First 3 letters: {word[1:3]}", end='\n')
-
-print(f"Last 2 letters: {word[-2:]}", end='\n')
-
-print(f"Middle word: {word[1:-1]}", end='\n')
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
