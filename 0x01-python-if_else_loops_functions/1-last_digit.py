24e4652049f50e97e43b67c0729fdd6374322f71
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-08 18:45:40 -0700

    1-last_digit.py changes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 import random
-
 number = random.randint(-10000, 10000)
-abs_number = abs(number)
-last_digit1 = abs_number % 10
-last_digit = abs(last_digit1)
-
-output = "Last digit of {}".format(number)
-output += " is {}".format(last_digit)
-if last_digit < 0:
-    last_digit = -last_digit
-if last_digit > 5:
-    output += " and is greater than 5"
-elif last_digit == 0:
-    output += " and is 0"
+lastdigit2 = abs(number) % 10
+if number < 0:
+    lastdigit2 = -digit
+print("Last digit of {} is {} and is ".format(number, lastdigit2), end="")
+if lastdigit2 > 5:
+    print("greater than 5")
+elif lastdigit2 == 0:
+    print("0")
 else:
-    output += " and is less than 6 and not 0"
-
-print(output)
+    print("less than 6 and not 0")
