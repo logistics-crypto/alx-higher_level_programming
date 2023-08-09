311ed94a5ddbb38d318bb02a8dec4cc92ba0c67c
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-08 18:28:41 -0700

    3 files requirement edit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3                                                                                                         
+#!/usr/bin/python3 
 alpha = range(97, 123)
 for i in range(97, 123):
     if i != 113 and i != 101:
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 hexnum = range(0, 99)
 for i in range(0, 99):
-    print("{0:d} = {0:x}".format(i))
+    print('{0:#d} = {0:#x}'.format(i))
