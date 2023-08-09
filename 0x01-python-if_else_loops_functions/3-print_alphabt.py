35b4984b24c0e01e539cb37d325ee7c3a7b136f3
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-08 18:35:00 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 alpha = range(97, 123)
 for i in range(97, 123):
     if i != 113 and i != 101:
-       print(chr(i), end="".format(i))
+        print(chr(i), end="".format(i))
