9e94832a049ebf5f958d8f0476eb314d107d2a06
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-08 18:16:15 -0700

    edit

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3                                                                                                         
-number = range(0, 99)
+#!/usr/bin/python3
+hexnum = range(0, 99)
 for i in range(0, 99):
     print("{0:d} = {0:x}".format(i))
