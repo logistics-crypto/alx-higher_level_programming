58a8307ffc26b928ec96bf510941666646dab2bb
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-08 18:13:13 -0700

    3 files

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,9 @@ import random
 
 number = random.randint(-10000, 10000)
 abs_number = abs(number)
-last_digit = abs_number % 10
+last_digit1 = abs_number % 10
+last_digit = abs(last_digit1)
+
 output = "Last digit of {}".format(number)
 output += " is {}".format(last_digit)
 if last_digit < 0:
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3                                                                                                         
+number = range(0, 99)
+for i in range(0, 99):
+    print("{0:d} = {0:x}".format(i))
