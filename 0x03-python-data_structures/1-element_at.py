12e83b1f2b5afbc521dd4b54e37fb06760bc3595
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-14 16:22:58 -0700

    2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -7,4 +7,4 @@ def element_at(my_list, idx):
 
 my_list = [1, 2, 3, 4, 5]
 idx = 3
-print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
+print("Element at index {:d} is {}\n".format(idx, element_at(my_list, idx)))
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
    if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
-    return (my_list)
+        return (my_list)
 
 my_list = [1, 2, 3, 4, 5]
 idx = 3
