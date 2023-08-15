533a2d4fd0120e67e8f96ee6f2fe02de57f8877d
logistics-crypto <logistics.aliouxx@gmail.com>
2023-08-15 23:44:34 +0000

    9-max_integer.py22

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,5 +7,5 @@ def max_integer(my_list=[]):
     high = my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > high:
-           high = my_list[i]
+            high = my_list[i]
     return (high)
